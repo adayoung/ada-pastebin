360c7a57239d0f348c30ce916fb9435341ee1983
Ada Young <ada@ada-young.com>
2024-11-20 04:02:20 +0100

    Wow, rust is complicated

--- src/utils.rs
+++ src/utils.rs
@@ -10,25 +10,60 @@ pub async fn extra_sugar(request: Request, next: Next) -> Result<impl IntoRespon
     let mut response = next.run(request).await;
 
     let mut sugar = vec![
-        ("Ada", "*skips about* Hi! <3 ^_^"),
-        ("X-Content-Type-Options", "nosniff"),
-        ("Referrer-Policy", "strict-origin-when-cross-origin"),
-        ("Strict-Transport-Security", "max-age=31536000"),
-        ("Permissions-Policy", "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()"),
+        ("Ada", String::from("*skips about* Hi! <3 ^_^")),
+        ("X-Content-Type-Options", String::from("nosniff")),
+        (
+            "Referrer-Policy",
+            String::from("strict-origin-when-cross-origin"),
+        ),
+        (
+            "Strict-Transport-Security",
+            String::from("max-age=31536000"),
+        ),
     ];
+
+    // Add Permissions-Policy header
+    sugar.push(("Permissions-Policy", generate_permissions_policy()));
+
+    // Add Content-Security-Policy header
+    sugar.push(("Content-Security-Policy", generate_csp()));
 
     if let Some(user_agent) = headers.get("User-Agent") {
         if user_agent.to_str().unwrap().contains("msie") {
-            sugar.push(("X-UA-Compatible", "IE=edge,chrome=1"));
-            sugar.push(("X-XSS-Protection", "1; mode=block"));
+            sugar.push(("X-UA-Compatible", String::from("IE=edge,chrome=1")));
+            sugar.push(("X-XSS-Protection", String::from("1; mode=block")));
         }
     }
 
     for (key, value) in sugar {
         response
             .headers_mut()
-            .insert(key, HeaderValue::from_static(value));
+            .insert(key, HeaderValue::from_str(&value).unwrap());
     }
 
     Ok(response)
+}
+
+fn generate_permissions_policy() -> String {
+    let permissions = vec![
+        "accelerometer=()",
+        "camera=()",
+        "geolocation=()",
+        "gyroscope=()",
+        "magnetometer=()",
+        "microphone=()",
+        "payment=()",
+        "usb=()",
+        "interest-cohort=()",
+    ];
+
+    permissions.join(",")
+}
+
+fn generate_csp() -> String {
+    let csp: Vec<&str> = vec![
+        // TODO: Add the CSP stuff here
+    ];
+
+    csp.join(";")
 }
