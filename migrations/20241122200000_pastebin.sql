a92e1a98a563c26ceb0b73203033c65eab6fcbb2
Ada Young <ada@ada-young.com>
2024-11-24 05:24:12 +0100

    We haf indexes!

--- migrations/20241122200000_pastebin.sql
+++ migrations/20241122200000_pastebin.sql
@@ -10,3 +10,7 @@ CREATE TABLE IF NOT EXISTS "pastebin" (
 	"gdriveid" varchar(384),
 	"rcscore" numeric(2, 1) CHECK (rcscore >= 0.0 AND rcscore <= 1.0)
 );
+
+CREATE INDEX IF NOT EXISTS paste_id_index ON pastebin(paste_id);
+CREATE INDEX IF NOT EXISTS tags_index ON pastebin USING GIN ("tags");
+CREATE INDEX IF NOT EXISTS date_tags_index ON pastebin(tags, date);
