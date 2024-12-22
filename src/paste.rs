adayoung/ada-pastebin

commit 325373c6cbae9f6cdda71297da9f002814ab0f71

We ken use an enum in the form!

src/paste.rs
@@ -8,7 +8,7 @@ use bigdecimal::BigDecimal;
 use chrono::Utc;
 use num_traits::FromPrimitive;
 use rand::Rng;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use sqlx::postgres::PgPool;
 use sqlx::types::chrono::DateTime;
 use sqlx::Error::RowNotFound;
@@ -89,7 +89,7 @@ pub async fn new_paste(
     }
 }
 
-#[derive(Serialize)]
+#[derive(Clone, Deserialize, Serialize)]
 #[serde(untagged)]
 pub enum PasteFormat {
     Text(String),
@@ -113,7 +113,7 @@ pub struct Paste {
     pub tags: Option<Vec<String>>,
     pub format: PasteFormat,
     pub date: DateTime<Utc>,
-    pub gdriveid: Option<String>, // Googe Drive object ID
+    pub gdriveid: Option<String>, // Google Drive object ID
     pub gdrivedl: Option<String>, // Google Drive download URL
     pub s3_key: String,
     pub rcscore: BigDecimal, // Recaptcha score
@@ -161,12 +161,7 @@ impl Paste {
             }
         }
 
-        let format = match form.format.as_str() {
-            "plain" => PasteFormat::Text(form.format.clone()),
-            "html" => PasteFormat::Html(form.format.clone()),
-            _ => return Err((StatusCode::BAD_REQUEST, "Invalid format".to_string())),
-        };
-
+        let format = form.format.clone();
         let rcscore = match BigDecimal::from_f64(score) {
             Some(score) => score,
             None => return Err((StatusCode::BAD_REQUEST, "Invalid score".to_string())),

src/forms.rs
@@ -1,3 +1,4 @@
+use crate::paste::PasteFormat;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)] // TODO: Remove Serialize
@@ -8,7 +9,7 @@ pub struct PasteForm {
     pub content: String,
     pub title: Option<String>,
     pub tags: Option<String>,
-    pub format: String,
+    pub format: PasteFormat,
     pub destination: String,
 }
 

commit f029496ed1fd15cc70209916f4bfc0b96a4b7f19

Oop, that was bug introduced in 325373c 🙈

src/paste.rs
@@ -91,6 +91,7 @@ pub async fn new_paste(
 }
 
 #[derive(Clone, Deserialize, Serialize)]
+#[serde(from = "String")]
 #[serde(untagged)]
 pub enum PasteFormat {
     Text(String),